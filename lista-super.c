53e66bfed286a65c603e22e54de1407a3db26849
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-06-16 00:21:21 -0300

    listaSuper inicializa bien

--- lista-super.c
+++ lista-super.c
@@ -2,7 +2,7 @@
 
 int main (){
 
-    char listaSuper = {"shampoo", "papel higienico", "pan", "jamon", "queso"};
+    char *listaSuper[] = {"shampoo", "papel higienico", "pan", "jamon", "queso"};
     
     for(int i = 0; i < 4; i++){
         printf("%s\n", listaSuper[i]);
